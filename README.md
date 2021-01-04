68194f9b2833fa93bbe81be64e0acc5112b0c791
Jen Doyle <jdoyle5254@users.noreply.github.com>
2021-01-03 19:38:52 -0600

    update employee & README

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # employee_tracker_w12
+
+## Project Title README Generator      [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Table of Contents:
+  1. [Description](#Description)
+  2. [Installation](#Installation)
+  3. [Usage](#Usage)
+  4. [Contributing](#Contributing)
+  5. [Licence](#Licence)
+  6. [Questions](#Questions) 
+
+## Description 
+This program was designed to allow people to create a professional README.md document for their project.The user would run the file from their terminal using node,and fill out the README questionnaire.The information is then displayed in each section of a generated README.md document.Included are links in the Table of Contents to each section and Links to the users GitHub and contact information. 
+
+## Installation
+ To install and run locally from Git Hub repository, got to the repository: https://github.com/Jdoyle5254/employee_tracker_w12
+ Click CODE box, select SSH and copy the repository to your clipboard. In your command-line navigate to the folder you want to hold the repository. Once there type 'git clone' and paste the repository information into the line. Once completed open the code in VS by typing the command code . This will take you to VS Code and you will be in the repository on your local machine.  Once cloned, create package.json using `npm init`.   
+
+## Usage 
+The program can be run from the Terminal from the script.js file included.The user should navigate to that file in the Terminal and the program can be run by node script.js.The user will be asked for various pieces of information to be updated or querried.  
+ 
+
+## Contributing - Credits
+ My Tutors and all others with most insightful information also W3 Schools and mySQL tutorials.
+ 
+
+## License
+MIT
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: 
+ 
+ The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. 
+ 
+ 	 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+## Questions 
+For more information on this project, here is a link to GitHub:
+[GitHub Link](https://github.com/jdoyle5254)
+
+Please reach out via email for any further questions:
+jen.doyle5254@gmail.com
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const mysql = require("mysql");
-const inquirer = require("inquirer"); 
+const inquirer = require("inquirer");
 const connection = require("./connection");
 
 
@@ -45,291 +45,304 @@ function readProducts() {
 
 startApp();
 
-function startApp(){
-inquirer
-.prompt([
-{
-  name: "action",
-  type: "list",
-  message: "What would you like to do?",
-  choices: [
-    "Add department, role, employee",
-    "View departments, roles, employees",
-    "Update employee roles",
-    "Exit"
-  ]
-}])
-.then((answer)  => {
-  switch (answer.action) {
-  case "Add department, role, employee":
-    addInformation();
-    break;
-
-    case "View departments, roles, employees":
-      viewCompany();
-      break;
-
-    case "Update employee roles":
-      updateEmployee();
-      break;
-
-    }
-});
-}
-
-function addInformation(){
+function startApp() {
   inquirer
-  .prompt ([
-    {
-    name: "selectAdd",
-    type: "checkbox",
-    message: "What information are you adding?",
-    choices: [
-     {   
-      name: "Add Department",
-     }, 
-     {
-       name: "Add Employee",
-     },
-     {
-       name: "Add Role", 
-     },
-    ]
-  }
-])
-
-  .then(function(answer){
-    console.log(answer)
-    switch(answer.selectAdd[0]) {
-      case "Add Department":
-      addDept();
-      break;
-
-      case "Add Employee":
-      addEmp(); 
-      break; 
-
-      case "Add Role":
-      addRole();
-      break; 
-
-
-    }
-
-  });
-}
-
-
-function addDept(){
-  inquirer
-  .prompt ([
-    {
-    name: "departmentName",
-    type: "input",
-    message: "Please add the department name?"
-  }
-])
-  .then(function(answer){
-    var query = connection.query (
-      "INSERT INTO department SET ?",
+    .prompt([
       {
-        department_name: answer.departmentName,
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "Add department, role, employee",
+          "View departments, roles, employees",
+          "Update employee roles",
+          "Exit"
+        ]
+      }])
+    .then((answer) => {
+      switch (answer.action) {
+        case "Add department, role, employee":
+          addInformation();
+          break;
+
+        case "View departments, roles, employees":
+          viewCompany();
+          break;
+
+        case "Update employee roles":
+          updateEmployee();
+          break;
+
       }
-    )
+    });
+}
+
+function addInformation() {
+  inquirer
+    .prompt([
+      {
+        name: "selectAdd",
+        type: "checkbox",
+        message: "What information are you adding?",
+        choices: [
+          {
+            name: "Add Department",
+          },
+          {
+            name: "Add Employee",
+          },
+          {
+            name: "Add Role",
+          },
+        ]
+      }
+    ])
+
+    .then(function (answer) {
+      console.log(answer)
+      switch (answer.selectAdd[0]) {
+        case "Add Department":
+          addDept();
+          break;
+
+        case "Add Employee":
+          addEmp();
+          break;
+
+        case "Add Role":
+          addRole();
+          break;
+
+
+      }
+
+    });
+}
+
+
+function addDept() {
+  inquirer
+    .prompt([
+      {
+        name: "departmentName",
+        type: "input",
+        message: "Please add the department name?"
+      }
+    ])
+    .then(function (answer) {
+      var query = connection.query(
+        "INSERT INTO department SET ?",
+        {
+          department_name: answer.departmentName,
+        }
+      )
+    })
+}
+
+function addEmp() {
+  inquirer
+    .prompt([
+      {
+        name: "first_name",
+        type: "input",
+        message: "Please add employee first name",
+
+      },
+      {
+        name: "last_name",
+        type: "input",
+        message: "Please add employee last name",
+      },
+      {
+        name: "role_id",
+        type: "input",
+        message: "Please add the employee role id",
+
+      },
+      {
+        name: "manager_id",
+        type: "input",
+        message: "Please add the employee's manager id",
+
+      },
+
+
+    ])
+
+
+    .then(function (answer) {
+      var query = connection.query(
+        "INSERT INTO employee SET ?",
+        {
+          first_name: answer.first_name,
+          last_name: answer.last_name,
+          role_id: answer.role_id,
+          manager_id: answer.manager_id,
+        }
+      )
+    })
+}
+
+
+function addRole() {
+  inquirer
+    .prompt([
+      {
+        name: "title",
+        type: "input",
+        message: "Please add the job title",
+
+      },
+      {
+        name: "salary",
+        type: "input",
+        message: "Please add the salary",
+      },
+      {
+        name: "department_id",
+        type: "input",
+        message: "Please add the department id for this title",
+
+      },
+
+    ])
+
+    .then(function (answer) {
+      var query = connection.query(
+        "INSERT INTO role SET ?",
+        {
+          job_title: answer.title,
+          salary: answer.salary,
+          department_id: answer.department_id,
+        }
+      )
+    })
+}
+
+function viewCompany() {
+  inquirer
+    .prompt([
+      {
+        name: "optView",
+        type: "checkbox",
+        message: "What information would you like to view?",
+        choices: [
+          {
+            name: "View Departments",
+          },
+          {
+            name: "View Employees",
+          },
+          {
+            name: "View Roles",
+          },
+
+        ]
+      }
+    ])
+    .then(function (answer) {
+      switch (answer.optView[0]) {
+        case "View Departments":
+          viewDept();
+          break;
+
+        case "View Employees":
+          viewEmp();
+          break;
+
+        case "View Roles":
+          viewRole();
+          break;
+      }
+
+    });
+}
+
+function viewDept() {
+  connection.query("SELECT * FROM department", function (err, res) {
+    if (err) throw err;
+    // console.log(res);  
+    console.table(res);
   })
 }
 
-function addEmp(){
-  inquirer 
-  .prompt ([
-    {
-    name: "first_name",
-    type: "input",
-    message: "Please add employee first name",
-  
-  },
-  {
-    name: "last_name",
-    type: "input",
-    message: "Please add employee last name",
-  },
-  {
-    name: "role_id",
-    type: "input",
-    message: "Please add the employee role id",
 
-  },
-  {
-    name: "manager_id",
-    type: "input",
-    message: "Please add the employee's manager id",
-
-  },
-
-
-])
-
-
-.then(function(answer){
-  var query = connection.query (
-    "INSERT INTO employee SET ?",
-    {
-      first_name: answer.first_name,
-      last_name: answer.last_name,
-      role_id: answer.role_id,
-      manager_id: answer.manager_id,
-    }
-  )
-})
-}
-
-
-function addRole(){
-  inquirer
-  .prompt ([
-    {
-    name: "title",
-    type: "input",
-    message: "Please add the job title",
-
-  },
-  {
-    name: "salary",
-    type: "input",
-    message: "Please add the salary",
-  },
-  {
-    name: "department_id",
-    type: "input",
-    message: "Please add the department id for this title",
-
-  },
-
-])
-
-.then(function(answer){
-  var query = connection.query (
-    "INSERT INTO role SET ?",
-    {
-      job_title: answer.title,
-      salary: answer.salary,
-      department_id: answer.department_id,
-    }
-  )
-})
-}
-
-function viewCompany(){
-  inquirer
-  .prompt ([
-    {
-      name: "optView",
-      type: "checkbox",
-      message: "What information would you like to view?",
-      choices: [
-        {
-          name: "View Departments",
-        },
-        {
-          name: "View Employees",
-        },
-        {
-          name: "View Roles",
-        },
-
-      ]
-    }
-  ])
-  .then(function(answer){
-    switch(answer.optView[0]) {
-      case "View Departments":
-      viewDept();
-      break;
-      
-      case "View Employees":
-      viewEmp(); 
-      break; 
-      
-      case "View Roles":
-      viewRole();
-      break;  
-    }
-    
-  });
-}
-
-  function viewDept(){ 
-    connection.query("SELECT * FROM department", function(err, res) {
-      if (err) throw err;        
-      // console.log(res);  
-      console.table(res); 
-  })
-}
-
-    
-  function viewEmp(){ 
-    connection.query("SELECT * FROM employee", function(err, res) {
-      if (err) throw err; 
-      // console.log(res)
-      console.table(res);       
+function viewEmp() {
+  connection.query("SELECT * FROM employee", function (err, res) {
+    if (err) throw err;
+    // console.log(res)
+    console.table(res);
   }
-    )}
+  )
+}
 
-    function viewRole(){ 
-      connection.query("SELECT * FROM role", function(err, res) {
-        if (err) throw err;        
-        // console.log(res);  
-        console.table(res); 
-    }
-      )}  
- //create a function that will pull the employee name as a list for the user to select to 
- //make changes
-  function updateEmployee(){
-    connection.query("SELECT id, first_name, last_name FROM employee", function(err, res) {
-       if (err) throw err;
-       console.log(res)
-       var names = [];
-       res.forEach(element => {
-         names.push({name : element.first_name + " " + element.last_name, value: element.id});
-       });
-       inquirer
-       .prompt ([
-         {
-          name: "change employee", 
+function viewRole() {
+  connection.query("SELECT * FROM role", function (err, res) {
+    if (err) throw err;
+    // console.log(res);  
+    console.table(res);
+  }
+  )
+}
+//create a function that will pull the employee name as a list for the user to select to 
+//make changes
+function updateEmployee() {
+  connection.query("SELECT id, first_name, last_name FROM employee", function (err, res) {
+    if (err) throw err;
+
+    console.log(res)
+    var names = [];
+    res.forEach(element => {
+      names.push({ name: element.first_name + " " + element.last_name, value: element.id });
+    });
+    inquirer
+      .prompt([
+        {
+          name: "changeEmployee",
           type: "list",
           message: "which employee would you like to update?",
           choices: names
         },
-      {
-        name: "options",
-        type: "checkbox",
-        message: "what information would you like to change?",
-        choices: [
-          {
-            name: "First Name",
-            value: "first_name",
-          }, 
-          {
-           name: "last_name",
-          },
-          {
-            name: "manager_id",
-          },
-          {
-            name: "role_id",
-          },
-        ]
-      },
-    {
-      name: "updated",
-      type: "input",
-      message: "Please update the field chosen"
-    }])
-    .then((answer) => {
-      var qry = "UPDATE employee Set "+ answer.options+"='" + answer.updated +"'  Where ?"
-      connection.query(qry,
-       function(err, res) {
-        if (err) throw err;
-        console.log(res)
-    })
-  }
+        {
+          name: "options",
+          type: "list",
+          message: "what information would you like to change?",
+          choices: [
+            {
+              name: "First Name",
+              value: "first_name",
+            },
+            {
+              name: "Last Name",
+              value: "last_name",
+            },
+            {
+              name: "manager Id",
+              value: "manager_id",
+            },
+            {
+              name: "Role ID",
+              value: "role_id",
+            },
+          ]
+        },
+        {
+          name: "updated",
+          type: "input",
+          message: "Please update the field chosen"
+        }])
+      .then((answer) => {
+        var qry = "";
+    console.log(answer)
+
+        if (answer.options == "last_name" || answer.options == "first_name")
+          qry = "UPDATE employee Set " + answer.options + "='" + answer.updated + "'  Where id =" + answer.changeEmployee;
+        else
+          qry = "UPDATE employee Set " + answer.options + "=" + answer.updated + "  Where id =" + answer.changeEmployee;
+        connection.query(qry, function (err, res) {
+          if (err) throw err;
+          console.log(res)
+        })
+      })
+  })
+}
